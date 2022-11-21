d70781b9387498d6849b37fb05a3c6b01ef1e3f2
Will-Switchd <85733714+Will-Switchd@users.noreply.github.com>
2022-11-21 16:43:25 +0000

    Use connection pooling

--- services/db.js
+++ services/db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2/promise');
 const config_private = require('../config_private');
 
 async function query(sql, params) {
-    const connection = await mysql.createConnection(config_private.db);
-    const [results, ] = await connection.execute(sql, params);
+    const pool = await mysql.createPool(config_private.db);
+    const [results, ] = await pool.execute(sql, params);
     return results;
 };
 
